46f25a282be7a8dedb7d6c2cb83cce1badd0e6b3
Duck2833 <elearningwebsite01@gmail.com>
2026-07-30 09:38:31 +0800

    improve footer

--- includes/footer.php
+++ includes/footer.php
@@ -50,17 +50,26 @@
 
                 <li>
                     <i class="fa-solid fa-location-dot"></i>
-                    9&9, 01Jalan Kempas Indah 1/3, Taman Kempas Indah, 81300 Skudai, Johor Darul Ta'zim
+                    <span>
+                        <a href="https://www.google.com/maps/place/Austive+Human+Capital+Sdn+Bhd/@1.5527241,103.7170736,19z/data=!4m15!1m8!3m7!1s0x31da6ff9abf70d61:0x8fa8eaf55d11c9d1!2sAustive+Human+Capital+Sdn+Bhd!8m2!3d1.5527228!4d103.7177173!10e1!16s%2Fg%2F11sxy6hpfg!3m5!1s0x31da6ff9abf70d61:0x8fa8eaf55d11c9d1!8m2!3d1.5527228!4d103.7177173!16s%2Fg%2F11sxy6hpfg?entry=ttu&g_ep=EgoyMDI2MDcyNy4wIKXMDSoASAFQAw%3D%3D"
+                        target="_blank">
+                        9&9, 01Jalan Kempas Indah 1/3, Taman Kempas Indah, 81300 Skudai, Johor Darul Ta'zim
+                    </a>
+                    </span>
                 </li>
 
                 <li>
                     <i class="fa-solid fa-phone"></i>
+                    <a href="tel:+60129021932">
                     +6012-902 1932
+                    </a>
                 </li>
 
                 <li>
                     <i class="fa-solid fa-envelope"></i>
-                    info@austive.com
+                    <a href="mailto:info@austive.com">
+                        info@austive.com
+                    </a>
                 </li>
 
             </ul>
@@ -99,7 +108,7 @@
 
     <div class="footer-bottom">
 
-        © 2026 Austive Human Capital Management.
+        © 2026 Austive Human Capital Sdn Bhd.
         All Rights Reserved.
 
     </div>
