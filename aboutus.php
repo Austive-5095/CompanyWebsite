bd563337eefcc0cb779c335044023a296ed308b4
Duck2833 <elearningwebsite01@gmail.com>
2026-08-03 12:27:56 +0800

    Revert "test"
    This reverts commit 3e4e7118efcb0d5744bc2bc76d11da35debfd5ca.

--- aboutus.php
+++ aboutus.php
@@ -1,10 +1,23 @@
 <?php
 
 include 'includes/header.php';
+include 'includes/navbar.php';
 
 ?>
+
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Austive Human Capital Sdn Bhd</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+
 <body>
-<?php include 'includes/navbar.php'; ?>
 
 <!-- ================= ABOUT US ================= -->
 
